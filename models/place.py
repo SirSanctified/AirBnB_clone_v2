a0fb1a33b9de940a539c8a7a129070fcb603247d
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-06 15:26:24 +0200

    Update place.py and amenity.py

--- models/place.py
+++ models/place.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
@@ -15,19 +16,17 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 class Place(BaseModel, Base):
     """ A place to stay """
-
-    __tablename__ = 'places'
-
+    __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer(), nullable=False, default=0)
-    number_bathrooms = Column(Integer(), nullable=False, default=0)
-    max_guest = Column(Integer(), nullable=False, default=0)
-    price_by_night = Column(Integer(), nullable=False, default=0)
-    latitude = Column(Float(), nullable=True)
-    longitude = Column(Float(), nullable=True)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     reviews = relationship("Review", backref='place', cascade="all, delete")
     amenities = relationship("Amenity", secondary=place_amenity,
                              viewonly=False)
