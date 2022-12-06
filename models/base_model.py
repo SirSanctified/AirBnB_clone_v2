2a5297894a0a95d91aa363293daf5401979ce8a7
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-06 12:12:37 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,19 +15,28 @@ class BaseModel:
     updated_at = Column(Datetime(), default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        # if not kwargs:
-        #     from models import storage
-        #     self.id = str(uuid.uuid4())
-        #     self.created_at = datetime.now()
-        #     self.updated_at = datetime.now()
-        #     storage.new(self)
-        if kwargs:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        """Instantiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                )
+            else:
+                self.updated_at = datetime.now()
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                )
+            else:
+                self.created_at = datetime.now()
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -57,4 +66,3 @@ class BaseModel:
         Delete the current instance from the storage (models.storage) by
         calling the method delete
         """
-        
