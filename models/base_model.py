8b9577a072f3d426a8879fbabbb2508e4efc2b4d
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-06 12:04:29 +0200

    [task #6] Implement database storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,27 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(Datetime(), default=datetime.utcnow(), nullable=False)
+    updated_at = Column(Datetime(), default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
+        # if not kwargs:
+        #     from models import storage
+        #     self.id = str(uuid.uuid4())
+        #     self.created_at = datetime.now()
+        #     self.updated_at = datetime.now()
+        #     storage.new(self)
+        if kwargs:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -31,6 +39,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -42,3 +51,10 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """
+        Delete the current instance from the storage (models.storage) by
+        calling the method delete
+        """
+        
---
+++ models/engine/db_storage.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+    """Database storage engine
+    """
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+import os
+
+user = os.getenv(HBNB_MYSQL_USER)
+password = os.getenv(HBNB_MYSQL_PWD)
+database = os.getenv(HBNB_MYSQL_DB)
+host = os.getenv(HBNB_MYSQL_HOST, default='localhost')
+env = os.getenv(HBNB_ENV)
+
+class DBStorage:
+    __classes = [State, City, User, Place, Review, Amenity]
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Contructor for the class DBStorage"""
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, password, host, database), pool_pre_ping=True)
+    if env == "test":
+        Base.MetaData.drop_all()
+    
+    def all(self, cls=None):
+        """Method to return a dictionary of objects"""
+        my_dict = {}
+        if cls in self.__classes:
+            result = DBStorage.__session.query(cls)
+            for row in result:
+                key = "{}.{}".format(row.__class__.__name__, row.id)
+                my_dict[key] = row
+        elif cls is None:
+            for cl in self.__classes:
+                result = DBStorage.__session.query(cl)
+                for row in result:
+                    key = "{}.{}".format(row.__class__.__name__, row.id)
+                    my_dict[key] = row
+        return my_dict
+    
+    def new(self, obj):
+        """add the object to the current database session 
+
+        Args:
+            obj (object): instance of any class from __classes
+        """
+
+        DBStorage.__session.add(obj)
+    
+    def save(self):
+        """commit all changes of the current database session 
+        """
+        DBStorage.__session.commit()
+    
+    def reload(self):
+        """create all tables in the database
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expires_on_commit=False)
+        Session = scoped_session(session_factory)
+        DBStorage.__session = Session()
+    
+    def close(self):
+        """Close the database session
+        """
+        DBStorage.__session.close()
