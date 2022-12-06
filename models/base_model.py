693023995da8537947522f551b7237e51404ca67
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-06 12:17:25 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -57,12 +57,17 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
 
     def delete(self):
         """
         Delete the current instance from the storage (models.storage) by
         calling the method delete
         """
+        from models import storage
+        storage.delete(self)
