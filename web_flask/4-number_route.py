19ec42c7b983dac3d676ab83ba2ec7bbe9238fd3
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-14 09:49:30 +0200

    Add specific route accepting an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Simple routing example
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c(text):
+    """Return  'C' followed by the value of the text variable"""
+    t = text.replace('_', ' ')
+    return 'C {}'.format(t)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python(text="is cool"):
+    """Add route accepting argument, but has a default value"""
+    t = text.replace('_', ' ')
+    return 'Python {}'.format(t)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Check if n is integer and return 'n is a number' if true
+    else return 404 page"""
+    if isinstance(n, (int)):
+        return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
