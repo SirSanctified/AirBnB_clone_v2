37fa5122e2f67095b5dca7c53af7d90fdd564a3d
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-14 08:51:34 +0200

    Add first flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Simple routing example
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return Hello HBNB!"""
+    return "Hello HBNB!"
