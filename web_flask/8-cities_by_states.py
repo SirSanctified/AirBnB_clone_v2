14e98a052c0408d4bdaf45add786fcaf6f61ccdf
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-14 22:31:51 +0200

    List cities by the states thet belong to

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+List cities by states
+"""
+
+from models import storage, State, City
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display cities by states"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
