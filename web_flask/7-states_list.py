5c1f2d82416041b248c9d01823ef206ee443c259
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-14 15:43:34 +0200

    Change 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """
-List all the states from the DBStorage engine
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/states_list: HTML page with a list of all State objects in DBStorage.
 """
-from models import storage
-from models.states import State
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_storage(exception):
-    """Remove the current SQLAlchemy Session after each request
-
-    Args:
-        exception (Exception): any exception
-    """
+def close_db(exc):
+    """close the current session of sqlalchemist"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """Render a template displaying a list of states available in storage
     """
-    return render_template('7-states_list.html', states=storage.all(State))
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
